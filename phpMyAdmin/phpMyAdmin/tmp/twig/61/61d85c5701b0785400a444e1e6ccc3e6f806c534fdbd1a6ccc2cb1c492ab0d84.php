ce4329a0ed2c4f9f3c9be538b206019bbf9a4860
poojitha1113 <poojitha.gutha1113@gmail.com>
2024-08-11 19:04:27 -0400

    nano project

---
+++ phpMyAdmin/phpMyAdmin/tmp/twig/61/61d85c5701b0785400a444e1e6ccc3e6f806c534fdbd1a6ccc2cb1c492ab0d84.php
@@ -0,0 +1,55 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* config/form_display/form_bottom.twig */
+class __TwigTemplate_f8e9ce250d874b93259e1c1e927ae8ded6abc09404e739da836f08707a87d5e2 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "</form>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "config/form_display/form_bottom.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "config/form_display/form_bottom.twig", "D:\\CSUSB\\NANO_INFO\\phpMyAdmin\\phpMyAdmin\\templates\\config\\form_display\\form_bottom.twig");
+    }
+}
